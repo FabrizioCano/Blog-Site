08ad2b349d322772412d6ef7dbe1bd00c085c7ae
FabrizioCano <fabriziocano7@gmail.com>
2024-07-17 11:24:46 -0400

    post and tailwind added

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+MEDIA_URL="/media/"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'feed',
+    'profiles',
+    'sorl.thumbnail',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
