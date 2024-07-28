5166248a43498a9feceb47667c5ca59e3eaef718
FabrizioCano <fabriziocano7@gmail.com>
2024-07-28 16:02:50 -0400

    followers started

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,10 +19,11 @@ from django.urls import path, re_path as url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
-
+from profiles import urls as profiles_urls
 from feed import urls as feed_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include(feed_urls,namespace="feed")),
+    path("profile/",include(profiles_urls,namespace="profiles")),
     url("",include("allauth.urls")),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
